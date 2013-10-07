90b2d9e26aad128cf59b6580073cf52d088573cd
g29670 <eliasM.2013@yahoo.com>
2013-10-06 22:49:29 -0500

    Working with jQuery and CSS.The VFW file's images are now all working.

---
+++ js/json.js
@@ -0,0 +1,222 @@
+var json = {
+	"movie1": {
+		"entermovie": ["Movie Style: ", "DVD"],
+		"mname": ["Movie Name: ", "Blade Runner"],
+		"mgenre": ["Movie Genre: ", "Action"],
+		"myear": ["Year movie was made: ", "01-01-1988"],
+		"mage": ["Suitable viewing age: ", "18"],
+		"favorite": ["Favorite: ", "Yes"],
+		"movierating": ["This movie is rated as a: ", "7"],
+		"quality": ["The movie quality is: ", "Excellent"],
+		"comments": ["This is what I think about this movie: ", "Comment on movie"]
+	},
+	"movie2": {
+		"entermovie": ["Movie Style: ", "VHS"],
+		"mname": ["Movie Name: ", "Hoagieville"],
+		"mgenre": ["Movie Genre: ", "Horror"],
+		"myear": ["Year movie was made: ", "02-02-1997"],
+		"mage": ["Suitable viewing age: ", "18"],
+		"favorite": ["Favorite: ", "No"],
+		"movierating": ["This movie is rated as a: ", "3"],
+		"quality": ["The movie quality is: ", "Good"],
+		"comments": ["This is what I think about this movie: ", "Comment on movie"]
+	},
+	"movie3": {
+	"entermovie": ["Movie Style: ", "Blu-ray"],
+		"mname": ["Movie Name: ", "Jokealot"],
+		"mgenre": ["Movie Genre: ", "Comedy"],
+		"myear": ["Year movie was made: ", "03-03-2011"],
+		"mage": ["Suitable viewing age: ", "21"],
+		"favorite": ["Favorite: ", "No"],
+		"movierating": ["This movie is rated as a: ", "5"],
+		"quality": ["The movie quality is: ", "Good"],
+		"comments": ["This is what I think about this movie: ", "Comment on movie"]
+	},
+	"movie4": {
+        "entermovie": ["Movie Style: ", "Blu-ray"],
+        "mname": ["Movie Name: ", "Assassin's Creed Linage"],
+        "mgenre": ["Movie Genre: ", "Action"],
+        "myear": ["Year movie was made: ", "01-01-2009"],
+        "mage": ["Suitable viewing age: ", "14"],
+        "favorite": ["Favorite: ", "Yes"],
+        "movierating": ["This movie is rated as a: ", "8"],
+        "quality": ["The movie quality is: ", "Excellent"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie5": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "Iron Man"],
+        "mgenre": ["Movie Genre: ", "Action"],
+        "myear": ["Year movie was made: ", "01-01-2008"],
+        "mage": ["Suitable viewing age: ", "13"],
+        "favorite": ["Favorite: ", "Yes"],
+        "movierating": ["This movie is rated as a: ", "8"],
+        "quality": ["The movie quality is: ", "Good"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie6": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "Perfect Stranger"],
+        "mgenre": ["Movie Genre: ", "Drama"],
+        "myear": ["Year movie was made: ", "01-01-2007"],
+        "mage": ["Suitable viewing age: ", "17"],
+        "favorite": ["Favorite: ", "No"],
+        "movierating": ["This movie is rated as a: ", "5"],
+        "quality": ["The movie quality is: ", ""],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie7": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "Dreamcatcher"],
+        "mgenre": ["Movie Genre: ", "Horror"],
+        "myear": ["Year movie was made: ", "01-01-2003"],
+        "mage": ["Suitable viewing age: ", "17"],
+        "favorite": ["Favorite: ", "No"],
+        "movierating": ["This movie is rated as a: ", "3"],
+        "quality": ["The movie quality is: ", "Good"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie8": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "The Visitation"],
+        "mgenre": ["Movie Genre: ", "Horror"],
+        "myear": ["Year movie was made: ", "01-01-2005"],
+        "mage": ["Suitable viewing age: ", "13"],
+        "favorite": ["Favorite: ", "No"],
+        "movierating": ["This movie is rated as a: ", "3"],
+        "quality": ["The movie quality is: ", "Good"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie9": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "Species III"],
+        "mgenre": ["Movie Genre: ", "Action"],
+        "myear": ["Year movie was made: ", "01-01-2004"],
+        "mage": ["Suitable viewing age: ", "17"],
+        "favorite": ["Favorite: ", "No"],
+        "movierating": ["This movie is rated as a: ", "4"],
+        "quality": ["The movie quality is: ", "Good"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie10": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "Jesus of Nazareth"],
+        "mgenre": ["Movie Genre: ", "Drama"],
+        "myear": ["Year movie was made: ", "01-01-1977"],
+        "mage": ["Suitable viewing age: ", "13"],
+        "favorite": ["Favorite: ", "Yes"],
+        "movierating": ["This movie is rated as a: ", "10"],
+        "quality": ["The movie quality is: ", "Excellent"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie11": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "Hotel Rwanada"],
+        "mgenre": ["Movie Genre: ", "Drama"],
+        "myear": ["Year movie was made: ", "01-01-2004"],
+        "mage": ["Suitable viewing age: ", "13"],
+        "favorite": ["Favorite: ", "Yes"],
+        "movierating": ["This movie is rated as a: ", "10"],
+        "quality": ["The movie quality is: ", "Good"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie12": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "Dare Devil"],
+        "mgenre": ["Movie Genre: ", "Action"],
+        "myear": ["Year movie was made: ", "01-01-2003"],
+        "mage": ["Suitable viewing age: ", "13"],
+        "favorite": ["Favorite: ", "No"],
+        "movierating": ["This movie is rated as a: ", "5"],
+        "quality": ["The movie quality is: ", "Good"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie13": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "M:i:III"],
+        "mgenre": ["Movie Genre: ", "Action"],
+        "myear": ["Year movie was made: ", "01-01-2006"],
+        "mage": ["Suitable viewing age: ", "13"],
+        "favorite": ["Favorite: ", "No"],
+        "movierating": ["This movie is rated as a: ", "6"],
+        "quality": ["The movie quality is: ", "Good"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie14": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "Gladiator"],
+        "mgenre": ["Movie Genre: ", "Action"],
+        "myear": ["Year movie was made: ", "01-01-2000"],
+        "mage": ["Suitable viewing age: ", "17"],
+        "favorite": ["Favorite: ", "Yes"],
+        "movierating": ["This movie is rated as a: ", "9"],
+        "quality": ["The movie quality is: ", "Excellent"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie15": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "The Bible"],
+        "mgenre": ["Movie Genre: ", "Drama"],
+        "myear": ["Year movie was made: ", "01-01-2005"],
+        "mage": ["Suitable viewing age: ", "13"],
+        "favorite": ["Favorite: ", "Yes"],
+        "movierating": ["This movie is rated as a: ", "10"],
+        "quality": ["The movie quality is: ", "Good"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie16": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "Elecktra"],
+        "mgenre": ["Movie Genre: ", "Action"],
+        "myear": ["Year movie was made: ", "01-01-2005"],
+        "mage": ["Suitable viewing age: ", "13"],
+        "favorite": ["Favorite: ", "No"],
+        "movierating": ["This movie is rated as a: ", "6"],
+        "quality": ["The movie quality is: ", "Good"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie17": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "Tears of the Sun"],
+        "mgenre": ["Movie Genre: ", "Action"],
+        "myear": ["Year movie was made: ", "01-01-2003"],
+        "mage": ["Suitable viewing age: ", "17"],
+        "favorite": ["Favorite: ", "Yes"],
+        "movierating": ["This movie is rated as a: ", "10"],
+        "quality": ["The movie quality is: ", "Excellent"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie18": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "The Breed"],
+        "mgenre": ["Movie Genre: ", "Action"],
+        "myear": ["Year movie was made: ", "01-01-2001"],
+        "mage": ["Suitable viewing age: ", "17"],
+        "favorite": ["Favorite: ", "No"],
+        "movierating": ["This movie is rated as a: ", "5"],
+        "quality": ["The movie quality is: ", "Good"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie19": {
+        "entermovie": ["Movie Style: ", "DVD"],
+        "mname": ["Movie Name: ", "Bless The Child"],
+        "mgenre": ["Movie Genre: ", "Horror"],
+        "myear": ["Year movie was made: ", "01-01-2000"],
+        "mage": ["Suitable viewing age: ", "17"],
+        "favorite": ["Favorite: ", "No"],
+        "movierating": ["This movie is rated as a: ", "4"],
+        "quality": ["The movie quality is: ", "Damaged"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    },
+    "movie20": {
+        "entermovie": ["Movie Style: ", "VHS"],
+        "mname": ["Movie Name: ", "O Brother, Where Art Thou?"],
+        "mgenre": ["Movie Genre: ", "Comedy"],
+        "myear": ["Year movie was made: ", "01-01-2000"],
+        "mage": ["Suitable viewing age: ", "13"],
+        "favorite": ["Favorite: ", "Yes"],
+        "movierating": ["This movie is rated as a: ", "7"],
+        "quality": ["The movie quality is: ", "Good"],
+        "comments": ["This is what I think about this movie: ", "Comment on movie"]
+    }
+};
